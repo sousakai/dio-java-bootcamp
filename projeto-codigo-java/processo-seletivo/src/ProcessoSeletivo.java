509db4639a9513e74351800153be81555c299767
Kayke <kayke.gsousa15@gmail.com>
2024-11-02 20:11:12 -0300

    Projetos - Java

--- projeto-codigo-java/processo-seletivo/src/ProcessoSeletivo.java
+++ projeto-codigo-java/processo-seletivo/src/ProcessoSeletivo.java
@@ -1,10 +1,51 @@
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
-
 //TODO: CASE 3 E CASE 4!
 
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
-        selecaoCandidatos();
+        String[] candidatos = { "Felipe", "Marcia", "Julia", "Paulo", "Augusto" };
+        for (String candidato : candidatos) {
+            entrandoEmContato(candidato);
+        }
+    }
+
+    // Método auxiliar
+    static boolean atender() {
+        return new Random().nextInt(3) == 1;
+    }
+
+    static void entrandoEmContato(String candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if (continuarTentando)
+                tentativasRealizadas++;
+            else
+                System.out.println("CONTATO REALIZADO COM SUCESSO");
+        } while (continuarTentando && tentativasRealizadas < 3);
+        if (atendeu)
+            System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + " TENTATIVA.");
+        else
+            System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato + ", NÚMERO MÁXIMO DE TENTATIVAS EXCEDIDO.");
+    }
+
+    static void imprimirSelecionados() {
+        String[] candidatos = { "Felipe", "Marcia", "Julia", "Paulo", "Augusto" };
+        System.out.println("Imprimindo a lista de candidatos informando o índice do elemento.");
+
+        for (int indice = 0; indice < candidatos.length; indice++) {
+            System.out.println("O candidato de nº" + (indice + 1) + " é " + candidatos[indice]);
+        }
+
+        System.out.println("Forma abreviada de interação 'for each'");
+
+        for (String candidato : candidatos) {
+            System.out.println("O candidato foi selecionado foi " + candidato);
+        }
     }
 
     static void selecaoCandidatos() {
